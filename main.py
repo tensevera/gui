62b7072b0dbd570528b1496d8a35c30cf1bfe4d2
Jan Severa <severja2@fit.cvut.cz>
2023-01-05 17:14:27 +0100

    Add zamichej

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import PySimpleGUI as sg
+import random
 
 # ''.join(random.sample(text,len(text)))
 
 WG_TEXT = "wg_text"
 WG_INPUT = "wg_input"
 WG_OTOC = "wg_otoc"
+WG_ZAMICHEJ = "wg_zamichej"
 
 # vzhled aplikace - vnorene seznamy - seznam radku a kazdy radek je seznam prvku (widgetu)
-vzhled = [[
-  sg.Text("Napiš text do pole podemnou a stiskni tlačítko Otoč.", key=WG_TEXT)
-], [sg.Input(key=WG_INPUT)], [sg.Button("Otoč", key=WG_OTOC)]]
+vzhled = [
+  [sg.Text("Napiš text do pole podemnou a stiskni tlačítko Otoč.", key=WG_TEXT)],
+  [sg.Input(key=WG_INPUT)],
+  [sg.Button("Otoč", key=WG_OTOC)],
+   [sg.Button("Zamichej", key=WG_ZAMICHEJ)]
+]
 
 window = sg.Window("Otoč", vzhled)
 
@@ -21,5 +26,9 @@ while True:
     text = values[WG_INPUT]
     otoceny = text[::-1]
     window[WG_TEXT].update(otoceny)
+  elif event == WG_ZAMICHEJ:
+    text = values[WG_INPUT]
+    zamichany = ''.join(random.sample(text,len(text)))
+    window[WG_TEXT].update(zamichany)
 
 window.close()
